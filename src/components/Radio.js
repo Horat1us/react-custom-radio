8d7a11d60214789846a2928394b822448b1ff342
Alexander Letnikow <reclamme@gmail.com>
2017-06-20 10:32:38 +0300

    Remove usage of React.PropTypes

--- src/components/Radio.js
+++ src/components/Radio.js
@@ -1,4 +1,4 @@
-import React, {Component, PropTypes} from 'react';
+import React, {Component} from 'react';
 import RadioContext from 'RadioContext';
 
 const Radio = class Radio extends Component {
